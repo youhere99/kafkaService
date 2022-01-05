2bd4bfcabc6ae4b978d1384f9dacffaa0cd3876e
zhaomingxing <zhaomingxing_zf@dhcc.com.cn>
2022-01-05 14:31:21 +0800

    使用SerializedLambda代替字符串

--- src/test/java/TestEscape.java
+++ src/test/java/TestEscape.java
@@ -1,6 +1,14 @@
+import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.ColumnCache;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.core.toolkit.support.SerializedLambda;
+import com.kafka.entity.CExLog;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.ibatis.reflection.property.PropertyNamer;
 import org.junit.Test;
+
+import java.util.Map;
 
 /**
  * @author zhaomingxing
@@ -18,5 +26,26 @@ public class TestEscape {
         sql="欧曼牌BJ4L67'MB-A";
         sql = StringEscapeUtils.escapeSql(sql);
         log.info(sql);
+    }
+
+    /**
+     * 获取类的属性名
+     */
+    @Test
+    public  void testLambda(){
+        /**
+         * 获取tablefiled字段名
+         */
+        Map<String, ColumnCache> columnMap = LambdaUtils.getColumnMap(CExLog.class);
+        getColumn(CExLog::getTableName,columnMap);
+    }
+
+    private String getColumn(SFunction<CExLog, ?> column, Map<String, ColumnCache> columnMap) {
+
+        SerializedLambda lambda = LambdaUtils.resolve(column);
+        //类的属性名
+        String fieldName = PropertyNamer.methodToProperty(lambda.getImplMethodName());
+        ColumnCache columnCache = columnMap.get(LambdaUtils.formatKey(fieldName));
+        return columnCache.getColumn();
     }
 }
